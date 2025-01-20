dea6727bca3fbec012a66c70947397a096b774f5
Isaac Hasselmann <isaacbilac920@gmail.com>
2025-01-20 11:18:50 -0300

    ADD gitignore e algumas pastas do projeto

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using Minimal_API.Dominio.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/login", (LoginDTO loginDTO) => 
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Login efetuado com sucesso!");
+    }
+    else
+    {
+        return Results.BadRequest("Usuário ou senha inválidos!");
+    }     
+});
+
 app.Run();
+
